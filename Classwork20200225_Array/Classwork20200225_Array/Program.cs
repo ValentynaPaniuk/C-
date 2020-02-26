098bdc52764c6fb9f753cc95a8c0c371a2f465e3
Valentyna Paniuk <Paniuk@rivne.itstep.org>
2020-02-26 12:56:25 +0200

    Array

--- Classwork20200225_Array/Classwork20200225_Array/Program.cs
+++ Classwork20200225_Array/Classwork20200225_Array/Program.cs
@@ -22,38 +22,38 @@ namespace Classwork20200225_Array
                 По даному Р программа повинна визначити, через скільки місяців розмір вкладу перевищить 1100 грн. 
                 та вивести знайдену кількість місяців К (ціле число) та підсумковий розмір вкладу S (дійсне число).  
             */
-            //Console.WriteLine(" *** Exercise 1 ***");
-            //int summa = 1000;
-            //int p;
-            //int i = 0;
-            //int month;
-            //bool exit = false;
+            Console.WriteLine(" *** Exercise 1 ***");
+            int summa = 1000;
+            int p;
+            int i = 0;
+            int month;
+            bool exit = false;
 
-            //Console.WriteLine("Enter %: ");
-            //p = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Enter month: ");
-            //month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter %: ");
+            p = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter month: ");
+            month = int.Parse(Console.ReadLine());
 
-            //while (i < month)
-            //{
-            //    summa += (summa * p / 100);
-            //    i++;
-            //    if (summa > 1100 && !exit )
-            //    {
-            //        Console.WriteLine("Amount exceeded 1100 through: ");
-            //        Console.WriteLine(i);
-            //        exit = true;
-            //    }
+            while (i < month)
+            {
+                summa += (summa * p / 100);
+                i++;
+                if (summa > 1100 && !exit)
+                {
+                    Console.WriteLine("Amount exceeded 1100 through: ");
+                    Console.WriteLine(i);
+                    exit = true;
+                }
 
-            //}
-                        
+            }
 
-            //Console.WriteLine("Summa new = ");
-            //Console.WriteLine(summa);
-            //Console.WriteLine("after ");
-            //Console.WriteLine(month);
-            //Console.WriteLine("months");
-            //Console.WriteLine("=====================================\n");
+
+            Console.WriteLine("Summa new = ");
+            Console.WriteLine(summa);
+            Console.WriteLine("after ");
+            Console.WriteLine(month);
+            Console.WriteLine("months");
+            Console.WriteLine("=====================================\n");
 
 
 
