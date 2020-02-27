b1f2bbf3637d94d60d3d19564533fcec9f615b7a
Valentyna Paniuk <Paniuk@rivne.itstep.org>
2020-02-27 14:56:32 +0200

    String

--- Classwork20200226_Array_part2/Classwork20200226_Array_part2/Program.cs
+++ Classwork20200226_Array_part2/Classwork20200226_Array_part2/Program.cs
@@ -29,123 +29,130 @@ namespace Classwork20200226_Array_part2
              */
             //Console.WriteLine(" *** Exercise 1 ***");
 
-            //int[] A = new int[5];
-            //for (int i = 0; i < A.Length; i++)
-            //{
-            //    A[i] = int.Parse(Console.ReadLine());
-            //}
+            int[] A = new int[5];
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
 
-            //Console.WriteLine("========== >>>>   Array A :");
-            //for (int i = 0; i < A.Length; i++)
+            Console.WriteLine("========== >>>>   Array A :");
+            for (int i = 0; i < A.Length; i++)
+            {
+                //Console.WriteLine($"A[{i}] = {A[i]}");
+                Console.Write(A[i] + " ");
+            }
+
+            Console.WriteLine("\n========== >>>>   Array A :");
+            float[,] B = new float[3, 4];
+            Random r = new Random();
+            for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
+                {
+                    B[i, j] = r.Next(0, 10);
+
+                }
+            }
+
+            Console.WriteLine("========= >>>>    Array B :");
+            for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
+                {
+                    Console.Write("{0} ", B[i, j]);
+
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("========= >>>>    Array B :");
+
+
+            Console.WriteLine(" ========== >>>>>  FIND MAX  :");
+
+
+            int size = 0;
+            int[] arr = new int[size];
+            for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
+                {
+                    for (int x = 0; x < A.Length; x++)
+                    {
+                        if (A[x] == B[i, j])
+                        {
+                            Console.WriteLine($"***A[{x}] = {A[x]}");
+                            arr[i] = A[i];
+                            size++;
+
+                        }
+                    }
+                }
+
+                Console.Write("\n");
+            }
+
+            Console.WriteLine($"size = {size}");
+            //for (int i = 0; i < size; i++)
             //{
             //    //Console.WriteLine($"A[{i}] = {A[i]}");
-            //    Console.Write(A[i] + " ");
+            //    Console.Write(arr[i] + " ");
             //}
 
-            //Console.WriteLine("\n========== >>>>   Array A :");
-            //float[,] B = new float[3, 4];
-            //Random r = new Random();
-            //for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
-            //{
-            //    for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
-            //    {
-            //        B[i, j] = r.Next(0, 10);
-
-            //    }
-            //}
-
-            //Console.WriteLine("========= >>>>    Array B :");
-            //for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
-            //{
-            //    for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
-            //    {
-            //        Console.Write("{0} ", B[i, j]);
-
-            //    }
-            //    Console.Write("\n");
-            //}
-            //Console.WriteLine("========= >>>>    Array B :");
 
 
-            //Console.WriteLine(" ========== >>>>>  FIND MAX  :");
+            Console.WriteLine(" <<<< ==========  FIND MAX");
+
+            int sum1 = 0;
+            float sum2 = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                sum1 += A[i];
+
+            }
+            for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
+                {
+                    sum2 += B[i, j];
 
 
-            //int size = 0;
-            //float[] arr = new float[size];
-            //for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
-            //{
-            //    for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
-            //    {
-            //        for (int x = 0; x < A.Length; x++)
-            //        {
-            //            if (A[x] == B[i, j])
-            //            {
-            //                Console.WriteLine($"***A[{x}] = {A[x]}");
-            //                size++;
+                }
+            }
 
-            //            }
-            //        }
-            //    }
+            Console.WriteLine($"Summa1 = {sum1}");
+            Console.WriteLine($"Summa2 = {sum2}");
 
-            //    Console.Write("\n");
-            //}
-
-            //Console.WriteLine($"size = {size}");
+            Console.WriteLine($" ========== >>>>>  ALL SUMMA   :  {sum1 + sum2}");
 
 
-            //Console.WriteLine(" <<<< ==========  FIND MAX");
+            int sumA = 0;
+            float sumB = 0;
 
-            //int sum1 = 0;
-            //float sum2 = 0;
-            //for (int i = 0; i < A.Length; i++)
-            //{
-            //    sum1 += A[i];
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sumA += A[i];
 
-            //}
-            //for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
-            //{
-            //    for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
-            //    {
-            //        sum2 += B[i, j];
+                }
+            }
 
+            Console.WriteLine($" ========== >>>>>  The sum of the even elements of an array A: {sumA}");
 
-            //    }
-            //}
+            for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        sumB += B[i, j];
 
-            //Console.WriteLine($"Summa1 = {sum1}");
-            //Console.WriteLine($"Summa2 = {sum2}");
+                    }
 
-            //Console.WriteLine($" ========== >>>>>  ALL SUMMA   :  {sum1 + sum2}");
+                }
+            }
 
-
-            //int sumA = 0;
-            //float sumB = 0;
-
-            //for (int i = 0; i < A.Length; i++)
-            //{
-            //    if (i % 2 == 0)
-            //    {
-            //        sumA += A[i];
-
-            //    }
-            //}
-
-            //Console.WriteLine($" ========== >>>>>  The sum of the even elements of an array A: {sumA}");
-
-            //for (int i = 0; i < B.GetUpperBound(0) + 1; i++)
-            //{
-            //    for (int j = 0; j < B.GetUpperBound(1) + 1; j++)
-            //    {
-            //        if (j % 2 != 0)
-            //        {
-            //            sumB += B[i, j];
-
-            //        }
-
-            //    }
-            //}
-
-            //Console.WriteLine($" ========== >>>>>  The sum of the odd columns in the array: {sumB} ");
+            Console.WriteLine($" ========== >>>>>  The sum of the odd columns in the array: {sumB} ");
 
 
 
@@ -155,110 +162,110 @@ namespace Classwork20200226_Array_part2
                1. Дан двумерный массив размерностью 5×5, заполненный случайными числами из диапазона от –100 до 100.
                   Определить сумму элементов массива, расположенных между минимальным и максимальным элементами.
              */
-            Console.WriteLine(" *** Exercise 2 ***");
-            int[,] C = new int[5, 5];
-            Random c = new Random();
-            for (int i = 0; i < C.GetUpperBound(0) + 1; i++)
-            {
-                for (int j = 0; j < C.GetUpperBound(1) + 1; j++)
-                {
-                    C[i, j] = c.Next(-10, 10);
+            //   Console.WriteLine(" *** Exercise 2 ***");
+            //   int[,] C = new int[5, 5];
+            //   Random c = new Random();
+            //   for (int i = 0; i < C.GetUpperBound(0) + 1; i++)
+            //   {
+            //       for (int j = 0; j < C.GetUpperBound(1) + 1; j++)
+            //       {
+            //           C[i, j] = c.Next(-10, 10);
 
-                }
-            }
+            //       }
+            //   }
 
-            Console.WriteLine("========= >>>>    Array C :");
-            for (int i = 0; i < C.GetUpperBound(0) + 1; i++)
-            {
-                for (int j = 0; j < C.GetUpperBound(1) + 1; j++)
-                {
-                    Console.Write("{0}    ", C[i, j]);
+            //   Console.WriteLine("========= >>>>    Array C :");
+            //   for (int i = 0; i < C.GetUpperBound(0) + 1; i++)
+            //   {
+            //       for (int j = 0; j < C.GetUpperBound(1) + 1; j++)
+            //       {
+            //           Console.Write("{0}    ", C[i, j]);
 
-                }
-                Console.Write("\n");
-            }
-            Console.WriteLine("========= >>>>    Array C :");
+            //       }
+            //       Console.Write("\n");
+            //   }
+            //   Console.WriteLine("========= >>>>    Array C :");
 
-           
-            int z1 = 0;
-            int index_max_i = 0;
-            int index_max_j = 0;
-            int z2 = 100;
-            int index_min_i = 0;
-            int index_min_j = 0;
-            for (int i = 0; i < C.GetUpperBound(0) + 1; i++)
-            {
-                for (int j = 0; j < C.GetUpperBound(1) + 1; j++)
-                {
-                    if (z1 < C[i, j])
-                    {
-                        z1 = C[i, j];
-                        index_max_i = i; index_max_j = j;
-                        Console.Write("\n");
-                    }
-                    else if (z2 > C[i, j])
-                    {
-                        z2 = C[i, j];
-                        index_min_i = i;
-                        index_min_j = j;
 
-                    }
+            //   int z1 = 0;
+            //   int index_max_i = 0;
+            //   int index_max_j = 0;
+            //   int z2 = 100;
+            //   int index_min_i = 0;
+            //   int index_min_j = 0;
+            //   for (int i = 0; i < C.GetUpperBound(0) + 1; i++)
+            //   {
+            //       for (int j = 0; j < C.GetUpperBound(1) + 1; j++)
+            //       {
+            //           if (z1 < C[i, j])
+            //           {
+            //               z1 = C[i, j];
+            //               index_max_i = i; index_max_j = j;
+            //               Console.Write("\n");
+            //           }
+            //           else if (z2 > C[i, j])
+            //           {
+            //               z2 = C[i, j];
+            //               index_min_i = i;
+            //               index_min_j = j;
 
-                }
-                Console.Write("\n");
-            }
-            Console.Write($"MAX Element C[{index_max_i},{index_max_j}]= {C[index_max_i, index_max_j]};  MIN Element C[{index_min_i},{index_min_j}] = {C[index_min_i, index_min_j]}");
-            Console.Write("\n");
-            Console.Write("\n");
+            //           }
 
-            int[,] arr1 = new int[5,5];
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    if (i == 0)
-                    {
-                        arr1[0, j] = C[index_min_i, j];
+            //       }
+            //       Console.Write("\n");
+            //   }
+            //   Console.Write($"MAX Element C[{index_max_i},{index_max_j}]= {C[index_max_i, index_max_j]};  MIN Element C[{index_min_i},{index_min_j}] = {C[index_min_i, index_min_j]}");
+            //   Console.Write("\n");
+            //   Console.Write("\n");
 
-                    }
-                    else if (i == index_min_i)
-                    {
-                        arr1[index_min_i, j] = C[0, j];
-                    }
-                    else
-                    {
-                        arr1[i, j] = C[i, j];
-                    }
-                }
-            }
+            //   int[,] arr1 = new int[5,5];
+            //   for (int i = 0; i < 5; i++)
+            //   {
+            //       for (int j = 0; j < 5; j++)
+            //       {
+            //           if (i == 0)
+            //           {
+            //               arr1[0, j] = C[index_min_i, j];
 
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write($" ARR1 = {0}    ", arr1[i, j]);
+            //           }
+            //           else if (i == index_min_i)
+            //           {
+            //               arr1[index_min_i, j] = C[0, j];
+            //           }
+            //           else
+            //           {
+            //               arr1[i, j] = C[i, j];
+            //           }
+            //       }
+            //   }
 
-                }
-                Console.Write("\n");
-            }
+            //   for (int i = 0; i < 5; i++)
+            //   {
+            //       for (int j = 0; j < 5; j++)
+            //       {
+            //           Console.Write($" ARR1 = {0}    ", arr1[i, j]);
 
-            /*2. Заполнить массив случайными числами, вывести его на экран. 
-           Найти самую длинную последовательность чисел, упорядоченную по возрастанию. 
-           Вывести ее на экран. 
-           Если таких последовательностей несколько (самых длинных с одинаковой длиной), то вывести их все.
-         */
+            //       }
+            //       Console.Write("\n");
+            //   }
 
-            Random d = new Random();
-            int[] D = new int[15];
-            for (int i = 0; i < D.Length; i++)
-            {
-                D[i] = d.Next(10,50);
-            }
+            //   /*2. Заполнить массив случайными числами, вывести его на экран. 
+            //  Найти самую длинную последовательность чисел, упорядоченную по возрастанию. 
+            //  Вывести ее на экран. 
+            //  Если таких последовательностей несколько (самых длинных с одинаковой длиной), то вывести их все.
+            //*/
 
-            for (int i = 0; i < D.Length; i++)
-            {
-                Console.WriteLine($"D[{i}] = {D[i]}");
-            }
+            //   Random d = new Random();
+            //   int[] D = new int[15];
+            //   for (int i = 0; i < D.Length; i++)
+            //   {
+            //       D[i] = d.Next(10,50);
+            //   }
+
+            //   for (int i = 0; i < D.Length; i++)
+            //   {
+            //       Console.WriteLine($"D[{i}] = {D[i]}");
+            //   }
 
             //for (int i = 0; i < D.Length; i++)
             //{
---
+++ Classwork20200227_String/Classwork20200227_String/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Classwork20200227_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Вводится строка слов. Вывести слова в обратном порядке.  
+             */
+            //string s = Console.ReadLine();
+            //string reverseS = new string(s.ToArray().Reverse().ToArray());
+            //Console.WriteLine(reverseS);
+
+            /* Вставить в заданную позицию строки другую строку*/
+
+            //string a = "My name is ";
+            //Console.WriteLine(a);
+            //string b = "Valya";
+            //Console.WriteLine(b);
+            //Console.WriteLine($"Length: {a.Length} ");
+            //string aNew = a.Insert(11, b);
+            //Console.WriteLine(aNew);
+
+            ///*Найти в строке все заданные последовательности символов и заменить их другой последовательностью. (з клавіатури)  */
+            //string d = "I have some questions";
+            //bool X = d.Contains("have");
+            //Console.WriteLine($"Is text: {X}");
+            //d = d.Replace("have", Console.ReadLine());
+            //Console.WriteLine(d);
+
+            ///* Дана квадратная матрица, состоящая из букв. 
+            // * Найти в ней цепочку букв, составляющую заданное слово.*/
+            char[,] str1 = new char[3, 3] { { 'a', 'b', 'c' }, { 'd', 'i', 'f' }, { 'j', 'k', 'l' } };
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < str1.GetUpperBound(1) + 1; j++)
+                {
+                    Console.WriteLine($"{str1[i, j]}");
+                }
+            }
+       
+
+
+
+
+            /* Дана строка слов, разделенных пробелами. 
+              Между словами может быть несколько пробелов, в начале и конце строки также могут быть пробелы. 
+              Требуется преобразовать строку так, чтобы в ее начале и конце пробелов не было, а слова были разделены одиночным символом "*" (звездочка)   
+             */
+
+           
+
+
+        }
+    }
+}
