10211dee4391aae45305bcbb31f1eaabea3176f7
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2020-03-18 15:15:03 +0200

    Delegate

---
+++ Classwork20200317_Delegate_part2/MyArray.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10___Delegates
+{
+    class MyArray
+    {
+
+        public delegate void Changing();
+        private Changing changeArray;
+
+        public void RegisterToChange(Changing changeArray)
+        {
+            this.changeArray = changeArray;
+        }
+
+        int[] arr;
+
+        public MyArray(int size)
+        {
+            arr = new int[size];
+        }
+
+        public void Fill()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-10, 10);
+            }
+        }
+
+        public void Change()
+        {
+            changeArray();
+        }
+
+        public int SumNegative()
+        {
+            int sumNegative = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                    sumNegative += arr[i];
+            }
+            return sumNegative;
+        }
+
+        public int SumAll()
+        {
+            return arr.Sum();
+        }
+
+        public void ChangeToNull()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                    arr[i] = 0;
+            }
+        }
+
+        public void Sort()
+        {
+            Array.Sort(arr);
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"[{arr[i]}] ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Classwork20200317_Delegate_part2/Program.cs
+++ Classwork20200317_Delegate_part2/Program.cs
@@ -4,12 +4,147 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Classwork20200317_Delegate_part2
+namespace Lesson_10___Delegates
 {
     class Program
     {
+       /* Завдання:
+        Написати програму для виконання операцій з одновимірним масивом за допомогою делегатів.
+        Користувачу надається наступне меню для вибору типа операції з масивом:
+        1. обчислення значення
+        2. зміна масиву
+
+        Якщо користувач вибрав 1-й тип, вивести підменю вибору операції:
+        i. Обчислити кількість негативних елементів
+        ii. Визначити суму всіх елементів
+        iii. * Обрахувати кількість простих чисел
+
+
+        2-й тип:
+        i.Змінити всі негативні елементи на 0
+        ii.Відсортувати масив
+        iii. * Перемістити всі парні елементи на початку, відповідно не парні будуть в кінці.
+
+         Створити вказані вище методи та реалізувати вибір користувачем операції на
+         виконання без використання конструкцій if, switch та?:(тернарного) оператора.
+        Методи першого типу повинні повертати результат обчислення, в той час методи другого типу – void.
+        Реалізувати валідацію вибраного номера операції.*/
+
+
+        static MyArray arr;
+
         static void Main(string[] args)
         {
+            arr = new MyArray(10);
+            arr.Fill();
+            Menu(arr);
+        }
+        public static void Menu(MyArray array)
+        {
+
+            int choice = 0;
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.Clear();
+                Console.Write("1 - Calculate array\n2 - Change array\n3 - Show array\n0 - Exit\nYour choice: ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        CalculateMenu(array);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        ChangeMenu(array);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        array.Show();
+                        Console.ReadLine();
+                        break;
+                    case 0:
+                        exit = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        public static void CalculateMenu(MyArray array)
+        {
+            Console.Clear();
+            int choice = 0;
+            Console.Write("1 - Sum of negative elements\n2 - Sum of all elements\nYour choice: ");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.Clear();
+                    array.RegisterToChange(SumNegativeElements);
+                    array.Change();
+                    break;
+                case 2:
+                    Console.Clear();
+                    array.RegisterToChange(SumAllElements);
+                    array.Change();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void ChangeMenu(MyArray array)
+        {
+            Console.Clear();
+            int choice = 0;
+            Console.Write("1 - Change all elements to 0\n2 - Sort array\nYour choice: ");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.Clear();
+                    array.RegisterToChange(NegativeToNull);
+                    array.Change();
+                    break;
+                case 2:
+                    Console.Clear();
+                    array.RegisterToChange(SortArray);
+                    array.Change();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+        private static void SumNegativeElements()
+        {
+            Console.WriteLine($"Sum of negative's elements: {arr.SumNegative()}");
+        }
+
+        private static void SumAllElements()
+        {
+            Console.WriteLine($"Sum of all elements: {arr.SumAll()}");
+        }
+
+        private static void NegativeToNull()
+        {
+            arr.ChangeToNull();
+            Console.WriteLine("Negative elements changed to 0:");
+            arr.Show();
+        }
+
+        private static void SortArray()
+        {
+            arr.Sort();
+            Console.WriteLine("Sorted Array: ");
+            arr.Show();
         }
     }
 }
